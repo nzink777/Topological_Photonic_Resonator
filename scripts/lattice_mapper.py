e819558ef0c29c1aa8cfea4b33296e2352eeaa1a
Natasha Zink <nzink777@gmail.com>
2026-06-18 17:02:30 -0700

    lattice_mapper.py for 64-state lattice visualization
    This script visualizes a 64-sector lattice discretization for a waveguide using a 8x8 grid.

---
+++ scripts/lattice_mapper.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_64_lattice():
+    """
+    Visualizes the 64-sector lattice discretization for the waveguide.
+    """
+    # 8x8 grid represents the 64 states (2^6)
+    grid = np.arange(64).reshape(8, 8)
+    
+    plt.figure(figsize=(6, 6))
+    plt.imshow(grid, cmap='viridis', origin='upper')
+    plt.colorbar(label='State ID')
+    plt.title("Cornucopia 64-State Lattice Projection")
+    plt.grid(True, color='white', linestyle='-', linewidth=2)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+plot_64_lattice()
